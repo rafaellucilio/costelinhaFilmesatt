8a1fe5d087be6c1383e2ffcb94fd5e813bd1cc6d
rafaellucilio <rafael.alves149@etec.sp.gov.br>
2023-06-02 17:24:33 -0300

    1

---
+++ src/data/series.js
@@ -0,0 +1,66 @@
+const DATA = 
+[
+    {
+        id:"01",
+        nome:"La casa de papel",
+        nota:'4.9',
+        imagem:'lcdp.jpg'
+    },
+    {
+        id:"02",
+        nome:"Cyberpunk : edgerunners",
+        nota:'10.0',
+        imagem:'cp.jpg'
+    },
+    {
+        id:"03",
+        nome:"Apenas um show",
+        nota:'9.5',
+        imagem:'rs.jpeg'
+    },
+    {
+        id:"04",
+        nome:"Black mirror",
+        nota:'7.5',
+        imagem:'bm.jpg'
+    },
+    {
+        id:"05",
+        nome:"Demon slayer",
+        nota:'6.8',
+        imagem:'dm.jpg'
+    },
+    {
+        id:"06",
+        nome:"Naruto",
+        nota:'5.7',
+        imagem:'nara.jpe'
+    },
+    {
+        id:"07",
+        nome:"Round 6",
+        nota:'9.8',
+        imagem:'r6.jpg'
+    },
+    {
+        id:"08",
+        nome:"O incrivel mundo de gumball",
+        nota:'4.5',
+        imagem:'img.jpg'
+    },
+    {
+        id:"09",
+        nome:"The boys",
+        nota:'4.7',
+        imagem:'tb.jpeg'
+    },
+    {
+        id:"10",
+        nome:"Invencivel",
+        nota:'7.0',
+        imagem:'iv.jpg'
+    }
+    
+]
+
+export default DATA;
