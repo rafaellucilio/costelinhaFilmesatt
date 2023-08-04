07ba5d49568d105b56362687a97ab1d7659e00a8
rafael lucilio <rafael.alves149@etec.sp.gov.br>
2023-08-04 17:32:03 -0300

    screens

---
+++ src/screen/Details/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View,Text } from "react-native";
+
+export default function Details () {
+    return (
+        <View>
+        <Text>
+        Está é a pagina de detalhes
+        </Text>
+        </View>
+    )
+}
